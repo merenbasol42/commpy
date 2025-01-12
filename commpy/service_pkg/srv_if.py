28881210fac91a87c4d89545ef4d6f441f841a27
ustad <merenbasol@gmail.com>
2025-01-12 20:49:52 +0300

    SrvMsgI updated. Request and Response sturctures changed

--- commpy/service_pkg/srv_if.py
+++ commpy/service_pkg/srv_if.py
@@ -1,6 +1,21 @@
+# srv_if.py
 from ..msg_if import MsgI
 
+class Request(MsgI):
+    def __init__(self, **kwargs):
+        super().__init__()
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+class Response(MsgI):
+    def __init__(self, **kwargs):
+        super().__init__()
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
 class SrvMsgI:
-    def __init__(self):
-        self.request: MsgI
-        self.response: MsgI
+    class Request(Request):
+        pass
+
+    class Response(Response):
+        pass
