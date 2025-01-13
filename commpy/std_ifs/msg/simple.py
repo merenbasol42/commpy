e2f73538a4fc4d96dc841607ec803073d13602bb
ustad <merenbasol@gmail.com>
2025-01-14 01:24:29 +0300

    minor changes

--- commpy/std_ifs/msg/simple.py
+++ commpy/std_ifs/msg/simple.py
@@ -4,6 +4,6 @@ from typing import Generic, TypeVar
 T = TypeVar('T')
 
 class MTSimple(MsgI, Generic[T]):  # MTList generic bir sınıf
-    def __init__(self):
+    def __init__(self, data: T):
         self.data: T 
-        super().__init__()  
+        super().__init__(data=data)  
