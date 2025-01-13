9da0cf784b478f1ac9bbb28d392e58c12e804d7a
ustad <merenbasol@gmail.com>
2025-01-13 20:57:30 +0300

    minor changes

--- commpy/std_ifs/srv/__init__.py
+++ commpy/std_ifs/srv/__init__.py
@@ -2,12 +2,12 @@ from ...service_pkg.srv_if import SrvMsgI as _SrvMsgI
 
 class Add(_SrvMsgI):
     class Request(_SrvMsgI.Request):
-        def __init__(self, numbers: list[int] = list[int]):
+        def __init__(self, numbers: list[int] = []):
             self.numbers: list[int]
             super().__init__(numbers=numbers)
 
     class Response(_SrvMsgI.Response):
-        def __init__(self, sum: int = int):
+        def __init__(self, sum: int = 0):
             self.sum: int
             super().__init__(sum=sum)
 
